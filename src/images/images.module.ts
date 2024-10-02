0ab009fa8e84f3ecb0d22f7d6e45c4aad506b9f8
Jacobo Ossa <jaco2419@gmail.com>
2024-10-02 09:51:13 -0500

    Added typeorm module for respective entity

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import { Image } from './entities/image.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Image])],
   controllers: [ImagesController],
   providers: [ImagesService],
 })
--- src/orders_details/orders_details.module.ts
+++ src/orders_details/orders_details.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersDetailsService } from './orders_details.service';
 import { OrdersDetailsController } from './orders_details.controller';
+import { OrderDetails } from './entities/order_detail';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([OrderDetails])],
   controllers: [OrdersDetailsController],
   providers: [OrdersDetailsService],
 })
